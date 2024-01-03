cf1dcf07b17502bf62f7ae8185f570e25d0ff05d
sadpoteto <106029162+sadpoteto@users.noreply.github.com>
2024-01-03 18:03:17 +0100

    Add color change on hover

--- script.js
+++ script.js
@@ -3,11 +3,18 @@ const container = document.querySelector("#container");
 const size = 16
 let divs = [[], []]
 
+let currentColor = "black";
+
 for(let i = 0; i < size; i++) {
     for(let j = 0; j < size; j++) {
         divs[i,j] = document.createElement("div");
         divs[i,j].style.width = `${100/size}%`;
         divs[i,j].style.height = `${100/size}%`;
+
+        divs[i,j].addEventListener("mouseover", (e) => {
+            e.target.style.backgroundColor = currentColor;
+            console.log("hey")
+        })
         container.appendChild(divs[i,j]);
     }
 }
