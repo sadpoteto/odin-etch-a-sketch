fb2fabbd39cddacc460de8bca5bf42cd7261513d
sadpoteto <106029162+sadpoteto@users.noreply.github.com>
2024-01-03 18:17:53 +0100

    Add grid resizing UI / code

--- script.js
+++ script.js
@@ -1,20 +1,37 @@
 const container = document.querySelector("#container");
+const gsBtn = document.querySelector("#gridsize");
 
-const size = 16
+gsBtn.addEventListener("click", setGridsize);
+
+let size = 16
 let divs = [[], []]
 
 let currentColor = "black";
 
-for(let i = 0; i < size; i++) {
-    for(let j = 0; j < size; j++) {
-        divs[i,j] = document.createElement("div");
-        divs[i,j].style.width = `${100/size}%`;
-        divs[i,j].style.height = `${100/size}%`;
+function setGridsize() {
+    while(1) {
+        let input = prompt("Enter grid size (1~100): ", size);
+        if(input === "") break;
+        input = parseInt(input);
+        if(isNaN(input)) continue;
+        size = input;
+        container.textContent = "";
+        //replaces all children with a #text node, faster than emptying 
+        //innerHTML through the browser's parser
+        
+        for(let i = 0; i < size; i++) {
+            for(let j = 0; j < size; j++) {
+                divs[i,j] = document.createElement("div");
+                divs[i,j].style.width = `${100/size}%`;
+                divs[i,j].style.height = `${100/size}%`;
 
-        divs[i,j].addEventListener("mouseover", (e) => {
-            e.target.style.backgroundColor = currentColor;
-            console.log("hey")
-        })
-        container.appendChild(divs[i,j]);
+                divs[i,j].addEventListener("mouseover", (e) => {
+                    e.target.style.backgroundColor = currentColor;
+                    console.log("hey")
+                })
+                container.appendChild(divs[i,j]);
+            }
+        }
+        break;
     }
 }
