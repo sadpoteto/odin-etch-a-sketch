cfed5a46ee6fb0b0fcb429b87ba4155c85c2c895
sadpoteto <106029162+sadpoteto@users.noreply.github.com>
2024-01-03 17:54:53 +0100

    Set div size dynamically, add basic UI coloring

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ let divs = [[], []]
 for(let i = 0; i < size; i++) {
     for(let j = 0; j < size; j++) {
         divs[i,j] = document.createElement("div");
+        divs[i,j].style.width = `${100/size}%`;
+        divs[i,j].style.height = `${100/size}%`;
         container.appendChild(divs[i,j]);
     }
 }
